91656bdbd12bc8eefbf7d28ded1b5b6a8e704fb7
John Ferguson Smart <john.smart@wakaleo.com>
2020-05-18 15:06:06 +0100

    Refactoring

--- src/test/java/com/demo/HomePage.java
+++ src/test/java/com/demo/HomePage.java
@@ -2,9 +2,4 @@ package com.demo;
 
 public class HomePage {
     public static String URL = "https://www.monsterworksdemo.com/home/";
-
-    public HomePage createNewAccount() {
-
-        return this;
-    }
 }
